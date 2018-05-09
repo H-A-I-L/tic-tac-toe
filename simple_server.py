a8621dfcd9f440cf5178e18fd6f42e2f6e632d1e
Ahmed Shariff <ahmed.shariff@live.com>
2018-05-10 00:10:30 +0530

    added basic index.html, and server to handle html files

--- simple_server.py
+++ simple_server.py
@@ -1,20 +1,69 @@
 from socketserver import ThreadingMixIn
 import threading
 import time
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
+import os
 
-class Handler(BaseHTTPRequestHandler):
-
+class Handler(SimpleHTTPRequestHandler):
+    
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        message =  threading.currentThread().getName()
-        self.wfile.write(message.encode('utf-8'))
-        self.wfile.write('\n'.encode('utf-8'))
-        print("sleeping", message)
-        time.sleep(15)
-        print("waking up", message)
-        return
+        # This section was copied from the SimpleHTTPRequestHandler source of cpython
+        f = self.send_head()
+        if f:
+            try:
+                self.copyfile(f, self.wfile)
+            finally:
+                f.close()
+        else:
+            # handle other requests here 
+            self.send_response(200)
+            self.end_headers()
+            message =  threading.currentThread().getName()
+            self.wfile.write(message.encode('utf-8'))
+            self.wfile.write('\n'.encode('utf-8'))
+
+    # This was copied from the SimpleHTTPRequestHandler source code of cpython
+    def send_head(self):
+        """Common code for GET and HEAD commands.
+        This sends the response code and MIME headers.
+        Return value is either a file object (which has to be copied
+        to the outputfile by the caller unless the command was HEAD,
+        and must be closed by the caller under all circumstances), or
+        None, in which case the caller has nothing further to do.
+        """
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(301)
+                self.send_header("Location", self.path + "/")
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except IOError:
+            return None
+        try:
+            self.send_response(200)
+            self.send_header("Content-type", ctype)
+            fs = os.fstat(f.fileno())
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+            self.end_headers()
+            return f
+        except:
+            f.close()
+            raise
+
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
