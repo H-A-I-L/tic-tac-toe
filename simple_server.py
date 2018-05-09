8c48a5865e6bf2d646943a86061e00fa79174a19
Ahmed Shariff <ahmed.shariff@rocketmail.com>
2018-05-09 16:40:35 +0530

    basic server testing in python

---
+++ simple_server.py
@@ -0,0 +1,25 @@
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn
+import threading
+import time
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        message =  threading.currentThread().getName()
+        self.wfile.write(message)
+        self.wfile.write('\n')
+        print("sleeping", message)
+        time.sleep(15)
+        print("waking up", message)
+        return
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+if __name__ == '__main__':
+    server = ThreadedHTTPServer(('localhost', 8080), Handler)
+    print('Starting server, use <Ctrl-C> to stop')
+    server.serve_forever()
